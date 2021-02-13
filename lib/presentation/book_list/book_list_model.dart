a000a3aa83acf2664f288585c1138d103383f3f5
岡田佳佑 <k-okada@cmind-co.jp>
2021-02-13 20:49:02 +0900

    テスト送:wq 信

---
+++ lib/presentation/book_list/book_list_model.dart
@@ -0,0 +1,17 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_app/book.dart';
+
+class BookListModel extends ChangeNotifier {
+  List<Book> books = [];
+
+  //firestoreのメソッドを呼ぶ
+  //asyncを描かないとawaitは使えない
+  Future fetchBooks() async {
+    final docs = await Firestore.instance.collection('books').getDocuments();
+    //docsがbooksに変換(マッピング)される
+    final books = docs.documents.map((doc) => Book(doc['title'])).toList();
+    this.books = books;
+    notifyListeners();
+  }
+}
